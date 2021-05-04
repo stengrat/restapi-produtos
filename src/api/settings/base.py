8bf085752fc83c8ea5d256b6a071e3bb7a0875cf
Stengrat <ygorsa1998@hotmail.com>
2021-05-04 16:21:40 -0300

    Adicionado deploy

--- src/api/settings/base.py
+++ src/api/settings/base.py
@@ -134,8 +134,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
